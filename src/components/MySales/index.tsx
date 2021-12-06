2465de2a9d58630041d064f6d721c700dbfa7f57
Sasha <o.stolitenko@temabit.com>
2021-12-06 12:15:35 +0200

    feat: added page layout

--- src/components/MySales/index.tsx
+++ src/components/MySales/index.tsx
@@ -3,28 +3,44 @@ import './MySales.scss';
 
 export const MySales: FC = () => {
 	return (
-		<div className="box box-white my-sales">
-			<div className="my-sales-table line">
-				<h2>Грудень 2021</h2>
-				<span className="location">Оболонський пр. 21</span>
-				<div className="department line">
-					<span>Гастрономія + Лавка Традицій + Рибний відділ</span>
-					<span>1 113 грн</span>
+		<>
+			<div className="box box-white my-sales">
+				<div className="my-sales-table line">
+					<h2>Грудень 2021</h2>
+					<span className="location">Оболонський пр. 21</span>
+					<div className="department line">
+						<span>Гастрономія + Лавка Традицій + Рибний відділ</span>
+						<span>1 113 грн</span>
+					</div>
+					<div className="department line">
+						<span>Пекарня</span>
+						<span>632 333 грн</span>
+					</div>
+					<span className="location">Перемоги пр. 10</span>
+					<div className="department line">
+						<span>Пекарня</span>
+						<span>2 333 грн</span>
+					</div>
 				</div>
-				<div className="department line">
-					<span>Пекарня</span>
-					<span>632 333 грн</span>
+				<div className="my-sales-sum">
+					<span>Загальна сума виторгу:</span>
+					<span>358 566 грн</span>
 				</div>
-				<span className="location">Перемоги пр. 10</span>
-				<div className="department line">
-					<span>Пекарня</span>
+			</div>
+			<div className="box box-white my-sales">
+				<div className="my-sales-table line">
+					<h2>Листопад 2021</h2>
+					<span className="location">Оболонський пр. 21</span>
+					<div className="department line">
+						<span>Пекарня</span>
+						<span>2 333 грн</span>
+					</div>
+				</div>
+				<div className="my-sales-sum">
+					<span>Загальна сума виторгу:</span>
 					<span>2 333 грн</span>
 				</div>
 			</div>
-			<div className="my-sales-sum">
-				<span>Загальна сума виторгу:</span>
-				<span>358 566 грн</span>
-			</div>
-		</div>
+		</>
 	);
 };
--- src/pages/ActiveSales/index.tsx
+++ src/pages/ActiveSales/index.tsx
@@ -1,40 +1,18 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { CurrentSection } from '../../components/CurrentSection';
 import { EncourageMessage } from '../../components/EncourageMessage';
-import { FilterSwitches } from '../../components/FilterSwitches';
-import { MySales } from '../../components/MySales';
+// import { MySales } from '../../components/MySales';
 import { TableSales } from '../../components/TableSales';
 
 import './ActiveSales.scss';
 
-const propForSwitcher = () => [
-	{
-		value: '1',
-		defaultChecked: true,
-		label: 'мої продажі'
-	},
-	{
-		value: '0',
-		defaultChecked: false,
-		label: 'рейтинг'
-	}
-];
-
 export const ActiveSales: FC = () => {
-	const [checked, setChecked] = useState(true);
-
 	return (
 		<div className="box-active-sales">
 			<CurrentSection />
-			<FilterSwitches changeStatus={() => setChecked(!checked)} propSwitches={propForSwitcher()} />
-			{checked ? (
-				<MySales />
-			) : (
-				<>
-					<EncourageMessage />
-					<TableSales />
-				</>
-			)}
+			{/* <MySales /> */}
+			<EncourageMessage />
+			<TableSales />
 		</div>
 	);
 };
