e833f9383813c0a5533185a381126a748f2b30e2
Sasha <o.stolitenko@temabit.com>
2021-12-08 16:40:31 +0200

    feat: added FilterSwitches component

--- src/pages/ActiveSales/index.tsx
+++ src/pages/ActiveSales/index.tsx
@@ -1,18 +1,40 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { CurrentSection } from '../../components/CurrentSection';
 import { EncourageMessage } from '../../components/EncourageMessage';
 import { MySales } from '../../components/MySales';
 import { TableSales } from '../../components/TableSales';
+import { FilterSwitches } from '../../components/FilterSwitches';
 
 import './ActiveSales.scss';
 
+const propForSwitcher = () => [
+	{
+		value: '1',
+		defaultChecked: true,
+		label: 'мої продажі'
+	},
+	{
+		value: '0',
+		defaultChecked: false,
+		label: 'рейтинг'
+	}
+];
+
 export const ActiveSales: FC = () => {
+	const [checked, setChecked] = useState(true);
+
 	return (
 		<div className="box-active-sales">
 			<CurrentSection />
-			<MySales />
-			<EncourageMessage />
-			<TableSales />
+			<FilterSwitches changeStatus={() => setChecked(!checked)} propSwitches={propForSwitcher()} />
+			{checked ? (
+				<MySales />
+			) : (
+				<>
+					<EncourageMessage />
+					<TableSales />
+				</>
+			)}
 		</div>
 	);
 };
